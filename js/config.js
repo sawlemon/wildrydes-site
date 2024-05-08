e31698c50ab83e388d449e7c73ea0dd0d1d2d632
solomonraj-a-presidio <98028068+solomonraj-a-presidio@users.noreply.github.com>
2024-05-09 00:06:56 +0530

    apigw invokeurl added

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_dFIZzGMMt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolClientId: 'oeguamvgc76tkb8ft4iao91me', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://kboadjcjrb.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
